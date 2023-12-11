2cf49f65fd02f9a13acf8c20e1b3451d0e216ed8
Derek Mercedes <mercedes.derek@gmail.com>
2023-12-11 14:20:13 -0700

    fib

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,14 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    out = []
+    i = 1
+    if length > 0:
+        out = [0]
+    while len(out) < length:
+        out.append(i)
+        if len(out) > 1 :
+            i = out[-1] + out[-2]
+    print(out)
+
+print_fibonacci(10)
